8fed9f713b44706bfc9a4abb52c6ecd84e75ff00
vamsiampolu <vamsideepak03@gmail.com>
2016-11-15 09:21:58 +1100

    extracts Book component to a seperate module and fixes linting issues

---
+++ app/Book.js
@@ -0,0 +1,81 @@
+import React, { PropTypes } from 'react';
+import { style } from 'glamor';
+import { CardHeader, CardMedia, CardText, CardActions, Card } from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import LoremPixel from './LoremPixel';
+import LoremIpsum from './LoremIpsum';
+
+const { string } = PropTypes;
+
+export default function Book({ title, author, cover }) {
+  let Cover;
+  if (cover == null) {
+    Cover = LoremPixel;
+  } else {
+    Cover = (<img width="200" height="100" alt={title} src={cover} />);
+  }
+
+  const cardHeaderTextStyle = {
+    paddingRight: 0,
+  };
+
+  const containerStyle = {
+    height: '16em',
+    padding: 0,
+    position: 'relative',
+  };
+
+  const titleStyle = {
+    fontSize: 13,
+  };
+
+  const headerRootStyle = {
+    padding: 8,
+  };
+
+  const actionsStyle = {
+    padding: 4,
+    position: 'absolute',
+    bottom: '1%',
+    left: '49%',
+  };
+
+  const authorStyle = style({
+    fontSize: 12,
+    paddingLeft: 8,
+    position: 'relative',
+    bottom: 8,
+  });
+
+  const textStyle = {
+    fontSize: 10,
+    padding: 8,
+  };
+
+  return (
+    <Card containerStyle={containerStyle}>
+      <CardHeader
+        title={title}
+        style={headerRootStyle}
+        textStyle={cardHeaderTextStyle}
+        titleStyle={titleStyle}
+      />
+      <span className={authorStyle}>{author}</span>
+      <CardMedia>
+        <Cover width={200} height={100} />
+      </CardMedia>
+      <CardText style={textStyle}>
+        <LoremIpsum />
+      </CardText>
+      <CardActions style={actionsStyle}>
+        <FlatButton label="Buy Now" />
+      </CardActions>
+    </Card>
+  );
+}
+
+Book.propTypes = {
+  title: string,
+  author: string,
+  cover: string,
+};
--- app/Bookshelf.js
+++ app/Bookshelf.js
@@ -1,83 +1,10 @@
 import React, { PropTypes } from 'react';
-import { CardHeader, CardTitle, CardMedia, CardText, CardActions, Card } from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton';
 import { style, merge } from 'glamor';
 import uuid from 'node-uuid';
 import { col, row } from 'flexboxgrid.js';
-import LoremPixel from './LoremPixel';
-import LoremIpsum from './LoremIpsum';
+import Book from './Book';
 
 const { string, arrayOf, shape } = PropTypes;
-
-function Book({ title, author, cover }) {
-  let Cover;
-  if (cover == null) {
-    Cover = LoremPixel;
-  } else {
-    Cover = (<img width="200" height="100" alt={title} src={cover} />);
-  }
-
-  const cardHeaderTextStyle = {
-    paddingRight: 0,
-  };
-
-  const containerStyle = {
-    height: '20em',
-    padding: 0,
-  };
-
-  const titleStyle = {
-    fontSize: 13,
-  };
-
-  const headerRootStyle = {
-    padding: 8,
-  };
-
-  const actionsStyle = {
-    bottom: '2.5em',
-    left: '6.5em',
-  };
-
-  const authorStyle = style({
-    fontSize: 12,
-    paddingLeft: 8,
-    position: 'relative',
-    bottom: 8,
-  });
-
-  const textStyle = {
-    fontSize: 10,
-    padding: 8,
-  };
-
-  return (
-    <Card containerStyle={containerStyle}>
-      <CardHeader 
-        title={title} 
-        style={headerRootStyle} 
-        textStyle={cardHeaderTextStyle} 
-        titleStyle={titleStyle} 
-      />
-      <span className={authorStyle}>{author}</span>
-      <CardMedia>
-        <Cover width={200} height={100} />
-      </CardMedia>
-      <CardText style={textStyle}>
-        <LoremIpsum />
-      </CardText>
-      <CardActions style={actionsStyle}>
-        <FlatButton label="Buy Now" />
-      </CardActions>
-    </Card>
-  );
-}
-
-Book.propTypes = {
-  title: string,
-  author: string,
-  cover: string,
-};
 
 const colAuto = style(col());
 const dontExpand = style({
@@ -100,7 +27,7 @@ export default function Bookshelf({ books }) {
       className={colFixedWidth}
     >
       <Book {...book} />
-    </div>)
+    </div>),
     );
   }
 
