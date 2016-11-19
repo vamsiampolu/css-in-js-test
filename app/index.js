cd4eacdc5a2c62bf707eb75d77b6954df872f1c8
vamsiampolu <vamsideepak03@gmail.com>
2016-11-20 04:14:54 +1100

    Attempting to render a static site using the static site generator plugin

--- app/index.js
+++ app/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import { Router, browserHistory } from 'react-router';
+import { Router, browserHistory, createMemoryHistory } from 'react-router';
 import { render } from 'react-dom';
+import ReactDOMServer from 'react-dom/server';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import routes from './routes';
 
 injectTapEventPlugin();
 
-const root = document.getElementById('root');
-render(<Router routes={routes} history={browserHistory} />, root);
+if(typeof document !== 'undefined') {
+  const root = document.getElementById('root');
+  render(<Router routes={routes} history={browserHistory} />, root);
+}
+
+
+export default function staticRenderer(locals, callback) {
+  const history = createMemoryHistory();
+  const { assets } = locals
+  const location = history.location(locals.path);
+  match({ routes, location }, (error, redirectLocation, renderProps) => {
+    const html = ReactDOMServer.renderToString(<RoutingContext {...renderProps} />);
+    const renderedTemplate = template({html, assets});
+    callback(null, renderedTemplate);
+  });
+}
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,32 @@
 const webpack = require('webpack');
 const validator = require('webpack-validator');
 const path = require('path');
+const merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const template = require('html-webpack-template');
 const NpmInstallPlugin = require('npm-install-webpack-plugin');
-const DotEnvPlugin = require('webpack-dotenv-plugin');
+const StaticSitePlugin = require('static-site-generator-webpack-plugin');
 
 const APP_PATH = path.resolve('./app');
 const BUILD_PATH = path.resolve('./build');
 const SPEC_PATH = path.resolve('./spec');
 
+const NPM_LIFECYCLE_EVENT = process.env.npm_lifecycle_event;
+
 const HotModuleReplacementPlugin = webpack.HotModuleReplacementPlugin;
 
-module.exports = validator({
-  entry: `${APP_PATH}/index.js`,
+
+const base = {
+  entry: {
+    main: `${APP_PATH}/index.js`,
+  },
   output: {
     filename: 'bundle.js',
     path: BUILD_PATH,
     publicPath: '/css-in-js-test/',
   },
   devtool: 'eval-source-map',
-  devServer: {
-    inline: true,
-    hot: true,
-    host: process.env.HOST || '0.0.0.0',
-    port: process.env.PORT || 3000,
-    stats: 'errors-only',
-    historyApiFallback: true,
-    contentBase: BUILD_PATH,
-    watchOptions: {
-      aggregateTimeout: 300,
-      poll: 1000,
-    },
-  },
   module: {
-    preLoaders: [
-      {
-        test: /\.jsx?/,
-        loader: 'eslint',
-        include: [
-          APP_PATH,
-          SPEC_PATH,
-        ],
-      },
-    ],
     loaders: [
       {
         test: /\.jsx?/,
@@ -59,17 +42,34 @@ module.exports = validator({
       },
     ],
   },
+};
+
+const hmr = {
+  devServer: {
+    inline: true,
+    hot: true,
+    host: process.env.HOST || '0.0.0.0',
+    port: process.env.PORT || 3000,
+    stats: 'errors-only',
+    historyApiFallback: true,
+    contentBase: BUILD_PATH,
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000,
+    },
+  },
   plugins: [
     new HotModuleReplacementPlugin(),
-    new DotEnvPlugin({
-      sample: './.env.default',
-      path: './.env',
-    }),
     new NpmInstallPlugin({
       dev(module) {
         return (/(^babel-?.*|.*-plugin$|.*-loader)/).test(module);
       },
     }),
+  ],
+};
+
+const html = {
+  plugins: [
     new HtmlWebpackPlugin({
       inject: false,
       mobile: true,
@@ -77,4 +77,57 @@ module.exports = validator({
       template,
     }),
   ],
-});
+};
+
+const dev = {
+  module: {
+    preLoaders: [
+      {
+        test: /\.jsx?/,
+        loader: 'eslint',
+        include: [
+          APP_PATH,
+          SPEC_PATH,
+        ],
+      },
+    ],
+  },
+};
+
+const paths = [
+  '/',
+  '/python',
+  '/ruby',
+  '/javascript',
+  '/haskell',
+];
+
+const staticSite = {
+  plugins: [
+    new StaticSitePlugin('main', paths, {}),
+  ],
+  output: {
+    publicPath: '/css-in-js-test/',
+  },
+};
+
+let config;
+
+switch (NPM_LIFECYCLE_EVENT) {
+  case 'start':
+    config = merge(base, hmr, dev, html);
+    break;
+
+  case 'build':
+    config = merge(base, dev, html);
+    break;
+
+  case 'deploy':
+    config = merge(base, dev, staticSite);
+    break;
+
+  default:
+    config = merge(base, dev, html);
+}
+
+module.exports = validator(config);
