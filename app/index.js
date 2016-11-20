4890e50ef6bc6473b32bdb11e293603aa3ee9ef5
vamsiampolu <vamsideepak03@gmail.com>
2016-11-21 06:16:05 +1100

    Uses hashHistory instead of browserHistory for gh-pages, avoids need to generate static site

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, browserHistory } from 'react-router';
+import { Router, hashHistory } from 'react-router';
 import { render } from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import routes from './routes';
@@ -7,5 +7,5 @@ import routes from './routes';
 injectTapEventPlugin();
 
 const root = document.getElementById('root');
-render(<Router routes={routes} history={browserHistory} />, root);
+render(<Router routes={routes} history={hashHistory} />, root);
 
