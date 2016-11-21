db6340c35ba0295b1ce3cd9aeed54956c4ccd552
vamsiampolu <vamsideepak03@gmail.com>
2016-11-22 04:07:54 +1100

    [WIP] Opinionated Image component to smooth out loading of images

---
+++ app/Image.js
@@ -0,0 +1,187 @@
+import React, { Component, PropTypes } from 'react';
+import { style, keyframes, merge } from 'glamor';
+
+const { string, number, bool, object } = PropTypes;
+
+const defaultWrapperStyle = {
+  width: 200,
+  height: 200,
+  backgroundColor: 'white',
+  backgroundSize: 'contain',
+  backgroundRepeat: 'none',
+  boxSizing: 'border-box',
+  position: 'relative',
+};
+
+const PENDING = 'PENDING';
+const LOADING = 'LOADING';
+const LOADED = 'LOADED';
+
+export default class Image extends Component {
+  constructor(props) {
+    super(props);
+    if (props.src != null && typeof props.src === 'string') {
+      this.state = {
+        status: LOADING,
+      };
+    } else {
+      this.state = {
+        status: PENDING,
+      };
+    }
+    this.onLoad = this.onLoad.bind(this);
+  }
+
+  onLoad() {
+    this.setState({
+      status: LOADED,
+    });
+  }
+
+  render() {
+    const {
+      src,
+      width,
+      height,
+      alt,
+      loadingStyle,
+      failureStyle,
+      rounded,
+      circle,
+    } = this.props;
+    const mainWrapperStyle = style({
+      ...defaultWrapperStyle,
+      width,
+      height,
+    });
+
+    let wrapperStyle = {};
+    if (rounded) {
+      const roundedStyle = style({
+        borderRadius: '10%',
+        overflow: 'hidden',
+      });
+
+      wrapperStyle = merge(mainWrapperStyle, roundedStyle);
+    } else if (circle) {
+      const circularStyle = style({
+        borderRadius: '50%',
+        overflow: 'hidden',
+      });
+
+      wrapperStyle = merge(mainWrapperStyle, circularStyle);
+    } else {
+      wrapperStyle = mainWrapperStyle;
+    }
+
+    const defaultImageStyle = style({
+      opacity: 0,
+      transisition: 'opacity 150ms ease',
+    });
+
+    const loadedImageStyle = style({
+      opacity: 1,
+    });
+
+    let imageStyle = defaultImageStyle;
+
+    if (this.state.status === LOADED) {
+      imageStyle = merge(defaultImageStyle, loadedImageStyle);
+    } else {
+      imageStyle = defaultImageStyle;
+    }
+
+
+    let image;
+    if (alt != null) {
+      image = (<img
+        className={imageStyle}
+        src={src}
+        width={width}
+        height={height}
+        alt={alt}
+        onLoad={this.onLoad}
+      />);
+    } else {
+      image = (<img
+        className={imageStyle}
+        src={src}
+        width={width}
+        height={height}
+        role="presentation"
+        onLoad={this.onLoad}
+      />);
+    }
+
+    let statusIndicator = null;
+    if (this.state.status === LOADING) {
+      statusIndicator = (<div className={loadingStyle}></div>);
+    }
+
+    return (<div className={wrapperStyle}>
+      {statusIndicator}
+      {image}
+    </div>);
+  }
+}
+
+Image.propTypes = {
+  src: string.isRequired,
+  width: number.isRequired,
+  height: number.isRequired,
+  alt: string,
+  loadingStyle: object,
+  failureStyle: object,
+  circle: bool,
+  rounded: bool,
+};
+
+
+const crossArm = {
+  background: '#00',
+  width: 5,
+  height: 10,
+  position: 'absolute',
+  top: '30%',
+  left: '50%',
+};
+
+const failureStyle = style({
+  ...crossArm,
+  transform: 'rotate(45deg)',
+  ':before': {
+    ...crossArm,
+    content: '',
+    top: 0,
+    left: 0,
+    transform: 'rotate(-90deg)',
+  },
+});
+
+const ring = keyframes({
+  '0%': {
+    transform: 'rotate(0deg)',
+  },
+  '100%': {
+    transform: 'rotate(360deg)',
+  },
+});
+
+const loadingStyle = style({
+  position: 'absolute',
+  display: 'block',
+  width: 40,
+  height: 40,
+  top: '37%',
+  left: '37%',
+  borderRadius: 80,
+  boxShadow: '0 3px 0 0 #59ebff',
+  animation: `${ring} 1s linear infinite`,
+});
+
+Image.defaultProps = {
+  loadingStyle,
+  failureStyle,
+  circle: false,
+  rounded: true,
+};
---
+++ test/Image.js
@@ -0,0 +1,69 @@
+/* eslint-env node, mocha*/
+import React from 'react';
+import chai, { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+import { shallow, mount } from 'enzyme';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import Image from '../app/Image';
+
+chai.use(chaiEnzyme());
+chai.use(sinonChai);
+
+describe('Image', () => {
+  it('exists', () => {
+    expect(Image).to.not.equal(null);
+  });
+
+  it('renders', () => {
+    const wrapper = shallow(<Image />);
+    expect(wrapper).to.not.equal(null);
+  });
+
+  it('has default props for loadingGlyph, loadingPlaceholder, failurePlaceholder', () => {
+    const wrapper = mount(<Image
+      src="something.png"
+      width={400}
+      height={200}
+    />);
+
+    expect(wrapper.props().loadingStyle).to.not.equal(null);
+    expect(wrapper.props().failureStyle).to.not.equal(null);
+    expect(wrapper.props().circle).to.equal(false);
+    expect(wrapper.props().rounded).to.equal(true);
+  });
+
+
+  it('has a wrapper div, an indicator div and an img', () => {
+    const wrapper = shallow(<Image src="something.png" width={400} height={200} />);
+    expect(wrapper).to.have.tagName('div');
+    expect(wrapper.childAt(1)).to.have.tagName('img');
+    expect(wrapper.childAt(0)).to.have.tagName('div');
+  });
+
+  it('has a default state of PENDING', () => {
+    const wrapper = shallow(<Image width={400} height={200} />);
+    expect(wrapper.state()).to.deep.equal({ status: 'PENDING' });
+  });
+
+  it('has a state of LOADING if an src prop is supplied', () => {
+    const wrapper = shallow(<Image src="something.png" width={400} height={200} />);
+    const status = wrapper.state().status;
+    expect(status).to.equal('LOADING');
+  });
+
+  it('has a state of LOADED if a good src prop is supplied', () => {
+    const wrapper = shallow(<Image
+      src="http://lorempixel.com/200/200"
+      width={400}
+      height={200}
+    />);
+    const instance = wrapper.instance();
+    instance.onLoad = sinon.spy(instance.onLoad);
+    wrapper.update();
+    const status = wrapper.state().status;
+    expect(status).to.equal('LOADED');
+    expect(instance.onLoad).to.have.been.called;
+  });
+});
+
