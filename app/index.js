c4d1b0921031e79f814041163e3b19928ed7a3dc
vamsiampolu <vamsideepak03@gmail.com>
2016-11-30 05:39:52 +1100

    Adds a css reset using glamor/reset

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router, hashHistory } from 'react-router';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
+import 'glamor/reset';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import routes from './routes';
 
